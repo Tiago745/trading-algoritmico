13ecf110707d88a3f931cb71a5a77c4d5ecf1319
David Leati <davidleati12@gmail.com>
2025-08-03 20:30:49 -0300

    Construção da simulação de trading

--- data/historical_data.py
+++ data/historical_data.py
@@ -9,8 +9,8 @@ def get_historical_klines(symbol, interval, start_str=None, end_str=None):
     return client.get_historical_klines(symbol, interval, start_str, end_str)
 
 symbol = 'BTCUSDT'
-interval = Client.KLINE_INTERVAL_4HOUR
-start_str = '20 Jul, 2023'
+interval = Client.KLINE_INTERVAL_12HOUR
+start_str = '20 Jul, 2017'
 end_str = '20 Jul, 2025'
 
 klines = get_historical_klines(symbol, interval, start_str, end_str)
@@ -18,9 +18,9 @@ klines = get_historical_klines(symbol, interval, start_str, end_str)
 # Converte os dados para um DataFrame do Pandas
 df = pd.DataFrame(klines, 
                     columns=['timestamp', 'open', 'high', 'low', 'close', 'volume',
-                            #'close_time', 'quote_asset_volume', 'number_of_trades',
-                            #'taker_buy_base_asset_volume', 'taker_buy_quote_asset_volume',
-                            #'ignore'
+                            'close_time', 'quote_asset_volume', 'number_of_trades',
+                            'taker_buy_base_asset_volume', 'taker_buy_quote_asset_volume',
+                            'ignore'
                             ] )
 
 # Converte o timestamp para um formato legível
