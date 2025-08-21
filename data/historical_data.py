cf395f758e76919222273bb0a192486f40c54ad7
David Leati <davidleati12@gmail.com>
2025-08-21 17:17:37 -0300

    Criação de condição para entrada e saída e tradução dos outputs do modelo utilizando MAPIE

--- data/historical_data.py
+++ data/historical_data.py
@@ -8,8 +8,8 @@ client = Client(api_key='', api_secret='')
 def get_historical_klines(symbol, interval, start_str=None, end_str=None):
     return client.get_historical_klines(symbol, interval, start_str, end_str)
 
-symbol = 'XRPUSDT' 
-interval = Client.KLINE_INTERVAL_1HOUR
+symbol = 'SOLUSDT' 
+interval = Client.KLINE_INTERVAL_15MINUTE
 start_str = '20 Jul, 2017'
 end_str = '20 Jul, 2025'
 
