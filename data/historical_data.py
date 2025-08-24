62ea96b7a445aca5061dcdd4720790936bb56fe6
David Leati <davidleati12@gmail.com>
2025-08-24 03:18:16 -0300

    figs

--- data/historical_data.py
+++ data/historical_data.py
@@ -8,23 +8,24 @@ client = Client(api_key='', api_secret='')
 def get_historical_klines(symbol, interval, start_str=None, end_str=None):
     return client.get_historical_klines(symbol, interval, start_str, end_str)
 
-symbol = 'SOLUSDT' 
-interval = Client.KLINE_INTERVAL_15MINUTE
+symbols = ['SOLUSDT']
+interval = Client.KLINE_INTERVAL_1HOUR
 start_str = '20 Jul, 2017'
 end_str = '20 Jul, 2025'
 
-klines = get_historical_klines(symbol, interval, start_str, end_str)
+for symbol in symbols:
+    klines = get_historical_klines(symbol, interval, start_str, end_str)
 
-# Converte os dados para um DataFrame do Pandas
-df = pd.DataFrame(klines, 
-                    columns=['timestamp', 'open', 'high', 'low', 'close', 'volume',
-                            'close_time', 'quote_asset_volume', 'number_of_trades',
-                            'taker_buy_base_asset_volume', 'taker_buy_quote_asset_volume',
-                            'ignore'
-                            ] )
+    # Converte os dados para um DataFrame do Pandas
+    df = pd.DataFrame(klines, 
+                        columns=['timestamp', 'open', 'high', 'low', 'close', 'volume',
+                                'close_time', 'quote_asset_volume', 'number_of_trades',
+                                'taker_buy_base_asset_volume', 'taker_buy_quote_asset_volume',
+                                'ignore'
+                                ] )
 
-# Converte o timestamp para um formato legível
-df['timestamp'] = pd.to_datetime(df['timestamp'], unit='ms')
+    # Converte o timestamp para um formato legível
+    df['timestamp'] = pd.to_datetime(df['timestamp'], unit='ms')
 
-# Salva o DataFrame em um arquivo CSV
-df.to_csv(f'data/fechamentos/{symbol}_{interval}_data.csv', index=False)
+    # Salva o DataFrame em um arquivo CSV
+    df.to_csv(f'data/fechamentos/{symbol}_{interval}_data.csv', index=False)
